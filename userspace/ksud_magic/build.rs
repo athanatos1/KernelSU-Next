953656ba157e9f0ace73a6ab8ee662ae230057f2
athanatos1 <20610799+athanatos1@users.noreply.github.com>
2025-02-21 18:32:17 -0500

    Fix hardcode error

--- userspace/ksud_magic/build.rs
+++ userspace/ksud_magic/build.rs
@@ -15,7 +15,7 @@ fn get_git_version() -> Result<(u32, String), std::io::Error> {
         .trim()
         .parse()
         .map_err(|_| std::io::Error::new(std::io::ErrorKind::Other, "Failed to parse git count"))?;
-    let version_code = 12383 //hardcoded
+    let version_code = 12383; //hardcoded
 
     let version_name = String::from_utf8(
         Command::new("git")
